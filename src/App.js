c78bc272d436d15e49f091b7c766bfb5980239e0
gitSilent <97840080+gitSilent@users.noreply.github.com>
2022-10-07 14:45:30 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.css"
+import CatCont from "./CatCont";
+
+function App() {
+  
+    return(
+       <CatCont />
+    )
+
+  }
+
+export default App;
---
+++ src/CatCont/CatCont.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useState, useEffect } from 'react';
+
+
+function CatCont(){
+
+    const [src, setSrc]= useState(0);
+
+
+    let source;
+    // fetch('https://aws.random.cat/meow')
+    // .then((response) => {
+    //  return response.json();
+    // })
+    // .then((data) => {
+    //  let img = document.querySelector(".img-cat");
+    //  let a = document.querySelector(".p-link");
+    //  img.setAttribute('src',data.file );
+    //  a.textContent = `Cat from: ${data.file}`;
+    //  a.setAttribute('href', data.file)
+    
+    //  console.log(source);
+    // });
+
+    async function updateLink() {
+        let response = await fetch('https://aws.random.cat/meow')
+        let response1 = await response.json();
+        console.log(response1.file)
+        setSrc(response1.file)
+    }
+    useEffect(() => {
+        updateLink();
+      },[]);
+    //  getLink()
+     return(
+         <div className="cat-cont" >
+             <img className="img-cat" src = {src} alt='' />
+             <a href="" className="p-link"></a>
+             <button className="btn-change" onClick={()=>{updateLink()}}>GET NEW KITTY!</button>
+         </div>
+     )
+    
+}
+
+export default CatCont;
