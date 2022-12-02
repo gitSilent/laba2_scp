459b77723a5b7b03b339714de3044086ae1a3874
gitSilent <97840080+gitSilent@users.noreply.github.com>
2022-12-02 10:41:06 +0300

    with_another_api

--- src/CatCont/CatCont.jsx
+++ src/CatCont/CatCont.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 function CatCont(){
 
     const [src, setSrc]= useState(0);
+    const [state, setState]= useState(0);
 
 
     let source;
@@ -22,24 +23,41 @@ function CatCont(){
     //  console.log(source);
     // });
 
+    
+    
+    // async function updateLink() {
+    //     let response = await fetch('https://aws.random.cat/meow')
+    //     let response1 = await response.json();
+    //     console.log(response1.file)
+    //     setSrc(response1.file)
+    // }
+    // useEffect(() => {
+    //     updateLink();
+    //   },[]);
+    // //  getLink()
+    //  return(
+    //      <div className="cat-cont" >
+    //          <img className="img-cat" src = {'https://api.lorem.space/image/movie?w=150&h=220'} alt='' />
+    //          <a href="" className="p-link"></a>
+    //          <button className="btn-change" onClick={()=>{updateLink()}}>GET NEW KITTY!</button>
+    //      </div>
+    //  )
+    
+
     async function updateLink() {
-        let response = await fetch('https://aws.random.cat/meow')
-        let response1 = await response.json();
-        console.log(response1.file)
-        setSrc(response1.file)
+        setState(state+1);
+        setSrc(`https://api.lorem.space/image/movie?w=350&h=520&hash=${state}`)
     }
     useEffect(() => {
         updateLink();
       },[]);
-    //  getLink()
      return(
-         <div className="cat-cont" >
-             <img className="img-cat" src = {src} alt='' />
+         <div className="cat-cont" >    
+             <img className="img-cat" src = {src} alt=''/>
              <a href="" className="p-link"></a>
-             <button className="btn-change" onClick={()=>{updateLink()}}>GET NEW KITTY!</button>
+             <button className="btn-change" onClick={()=>{updateLink()} }>GET NEW KITTY!</button>
          </div>
      )
-    
 }
 
 export default CatCont;
